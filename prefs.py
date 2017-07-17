366d8675e5ce3e035ecb257a20e06a170557ed33
Rebecca Turner <637275@gmail.com>
2017-07-17 01:51:30 -0400

    api changes, weather, polishes

--- prefs.py
+++ prefs.py
@@ -5,7 +5,14 @@ def json_from_file(fname):
         return json.loads(f.read())
     return None
 
-def prefs(pref_path='prefs.json'):
+global prefs
+global keys
+
+def get_prefs(pref_path='prefs.json'):
+    global prefs
+    global keys
     prefs = json_from_file(pref_path)
     keys  = json_from_file(prefs['api_keys'])
     return prefs, keys
+
+load_prefs = get_prefs
