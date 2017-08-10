bb6243e22a7b88f283dcf2d2cc0e419ab4e74d48
Rebecca Turner <637275@gmail.com>
2017-08-10 18:07:53 -0400

    documentation, news fixes / updates

--- news.py
+++ news.py
@@ -18,6 +18,61 @@ def nytimes(postfix):
 def cnn(postfix):
     return f'http://rss.cnn.com/rss/cnn_{postfix}.rss'
 
+def npr(section):
+    sections = {
+        'News'         : '1001',
+        'News'         : '1002',
+        'World'        : '1004',
+        'Business'     : '1006',
+        'Science'      : '1007',
+        'Iraq'         : '1010',
+        'Politics'     : '1012',
+        'Education'    : '1013',
+        'Politics'     : '1014',
+        'Race'         : '1015',
+        'Religion'     : '1016',
+        'Economy'      : '1017',
+        'Technology'   : '1019',
+        'Media'        : '1020',
+        'Interviews'   : '1022',
+        'Environment'  : '1025',
+        'Space'        : '1026',
+        'Books'        : '1032',
+        'Music'        : '1039',
+        'Movies'       : '1045',
+        'Diversions'   : '1051',
+        'Food'         : '1053',
+        'Gardening'    : '1054',
+        'Sports'       : '1055',
+        'Opinion'      : '1057',
+        'Columns'      : '1058',
+        'Analysis'     : '1059',
+        'Remembrances' : '1062',
+        'Holidays'     : '1065',
+        'Law'          : '1070',
+        'Summer'       : '1088',
+        'Concerts'     : '1109',
+        'Governing'    : '1123',
+        'Europe'       : '1124',
+        'Asia'         : '1125',
+        'Africa'       : '1126',
+        'Health'       : '1128',
+        'Humans'       : '1129',
+        'Energy'       : '1131',
+        'Animals'      : '1132',
+        'History'      : '1136',
+        'Television'   : '1138',
+        'Recipes'      : '1139',
+        'Architecture' : '1142',
+        'Photography'  : '1143',
+        'Theater'      : '1144',
+        'Dance'        : '1145',
+        'Multimedia'   : '1147',
+        'Afghanistan'  : '1149',
+    }
+
+    return 'http://www.npr.org/rss/rss.php?id=' + sections[section]
+
 def feed_parser(url=reuters('worldnews')):
     if url not in cache:
         feed = requests.get(url)
@@ -34,12 +89,24 @@ def stories(url=reuters('worldnews')):
 def format_story(story):
     desc = BeautifulSoup(story.description.text, 'html.parser')
     head = BeautifulSoup(story.title.text, 'html.parser')
-    timefmt = '%a, %d %b %Y %H:%M:%S %z'
+
+    timefmtprefix = '%a, %d %b %Y %H:%M:%S'
     try:
-        pubdate = datetime.strptime(story.pubdate.text, timefmt)
+        pubdate = datetime.strptime(story.pubdate.text, timefmtprefix + ' %z')
     except ValueError:
-        timefmt = '%a, %d %b %Y %H:%M:%S %Z'
-        pubdate = datetime.strptime(story.pubdate.text, timefmt)
+        try:
+            # possibly a named timezone
+            pubdate = datetime.strptime(story.pubdate.text, timefmtprefix + ' %Z')
+        except ValueError:
+            try:
+                # fuck the timezone
+                pubdate = datetime.strptime(
+                    story.pubdate.text[:story.pubdate.text.rfind(' ')],
+                    timefmtprefix)
+            except ValueError:
+                # FUCK THE TIME
+                pubdate = datetime.min
+
     return extformat(prefs['news']['format'],
         description=desc.text,
         title=head.text,
